87f3df5b101e2b5b26d726a9a7e23c05af0e0884
Gennady Basov <g.a.basov@gmail.com>
2024-12-05 19:08:15 +0100

    05

---
+++ 05/part2.ts
@@ -0,0 +1,82 @@
+import { readFileSync} from 'fs'
+
+const input = readFileSync('input.txt', 'utf-8').trim()
+const inputParts = input.split('\n\n').map(text => text.split('\n'))
+const pairs = inputParts[0].map(pair => pair.split('|').map(num => parseInt(num)))
+const updates = inputParts[1].map(update => update.split(',').map(num => parseInt(num)))
+
+const rules: Map<number, Set<number>> = new Map()
+pairs.forEach(pair => {
+    const [left, right] = pair
+    let nums = rules.get(right)
+    if (!nums) {
+        nums = new Set()
+        rules.set(right, nums)
+    }
+    nums.add(left)
+})
+
+const validate = (update: number[]): boolean => {
+    if (update.length < 2) {
+        return true
+    }
+
+    const rule = rules.get(update[0])
+    const pages = update.toSpliced(0, 1)
+    if (rule) {
+        const incorrect = pages.some(page => rule.has(page))
+        if (incorrect) {
+            return false
+        }
+    }
+
+    return validate(pages)
+}
+
+const middle = (update: number[]): number => {
+    return update[Math.floor(update.length / 2)]
+}
+
+const swap = (arr: any[], i: number, j: number) => {
+    const k = arr[i]
+    arr[i] = arr[j]
+    arr[j] = k
+}
+
+const fix = (original: number[]): number[] => {
+    const fixed = [...original]
+
+    let i = 0
+    while (i < fixed.length - 1) {
+        const page = fixed[i]
+        const rule = rules.get(page)
+        if (!rule) {
+            i++
+            continue
+        }
+
+        let j = i + 1
+        let swapped = false
+        while (j < fixed.length) {
+            if (rule.has(fixed[j])) {
+                swap(fixed, i, j)
+                swapped = true
+                break
+            } else {
+                j++
+            }
+        }
+
+        if (!swapped) {
+            i++
+        }
+    }
+
+    return fixed
+}
+
+const sum = updates.reduce(
+    (sum, update) => validate(update) ? sum : sum + middle(fix(update)), 0
+)
+
+console.log(sum)
