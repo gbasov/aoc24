ecb99dadf4872278bd137b0e5fa2501783445df9
Gennady Basov <g.a.basov@gmail.com>
2024-12-19 23:03:12 +0100

    update template

--- tmpl/part2.ts
+++ tmpl/part2.ts
@@ -1,8 +1,9 @@
 console.time('Time')
 import { readFileSync } from 'fs'
-const IS_TEST = true
-// const IS_TEST = false
 
-const input = readFileSync(`${IS_TEST ? 'test' : 'input'}.txt`, 'utf-8').trim()
+const TEST = true
+// const TEST = false
+
+const input = readFileSync(`${TEST ? 'test' : 'input'}.txt`, 'utf-8').trim()
 
 console.timeEnd('Time')
